9301a37fe315c5d770dd149b30a7364438cba9a5
nklinh102 <nklinh102@gmail.com>
2025-11-09 20:21:29 +0700

    Fix import statements for DOMParser and Node

--- functions/save-data.js
+++ functions/save-data.js
@@ -1,9 +1,10 @@
 // /functions/save-data.js
 
-// === SỬA LỖI: Thay 'global' bằng 'self' cho Cloudflare ===
-import { DOMParser } from '@xmldom/xmldom';
+// === SỬA LỖI: Thêm 'Node' vào 'self' ===
+import { DOMParser, Node } from '@xmldom/xmldom'; // Import thêm Node
 self.DOMParser = DOMParser;
-// ======================================================
+self.Node = Node; // Thêm dòng này
+// ======================================
 
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
